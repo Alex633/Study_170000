e4b33b1b4c12957b32899e5bec258ce2fe063d8d
alex_k63 <38862874+Alex633@users.noreply.github.com>
2024-02-15 20:08:23 +0100

    Update 6.0.3_DataBaseOfGamers.cs

--- 6.0.3_DataBaseOfGamers.cs
+++ 6.0.3_DataBaseOfGamers.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
 
-//Реализовать базу данных игроков и методы для работы с ней.
-//У игрока может быть уникальный номер, ник, уровень, флаг – забанен ли он(флаг - bool).
-//Реализовать возможность добавления игрока, бана игрока по уникальный номеру,
-//разбана игрока по уникальный номеру и удаление игрока.
-//Создание самой БД не требуется, задание выполняется инструментами,
-//которые вы уже изучили в рамках курса. Но нужен класс, который содержит игроков и её можно назвать "База данных".
+// Implement a database of players and methods for working with it.
+// Each player may have a unique number, nickname, level, and a flag indicating if they are banned (a boolean flag).
+// Implement the ability to add a player, ban a player by their unique number,
+// unban a player by their unique number, and delete a player.
+// Creating the actual database is not required; the task is performed using tools
+// that you have already learned within the course. However, a class is needed that contains players, and it can be called a "Database".
 
 namespace CsRealLearning
 {
@@ -24,16 +24,16 @@ namespace CsRealLearning
         class Database
         {
             List<Player> players = new List<Player>();
+            private bool _yourFuneral = false;
 
             public void Work()
             {
-                while (true)
+                while (!_yourFuneral)
                 {
                     ShowCommands();
                     HandleCommand();
 
-                    Console.ReadKey();
-                    Console.Clear();
+                    Usability.PressAnything();
                 }
             }
             enum UserAction
@@ -41,12 +41,14 @@ namespace CsRealLearning
                 add_player = 1,
                 remove_player_by_id,
                 ban_player,
-                show_all_players
+                unban_player,
+                show_all_players,
+                get_back_to_real_life
             }
 
             private void ShowCommands()
             {
-                Console.WriteLine("Select a command:");
+                Console.WriteLine("Select a command:\n");
 
                 foreach (UserAction command in Enum.GetValues(typeof(UserAction)))
                 {
@@ -67,10 +69,16 @@ namespace CsRealLearning
                             RemovePlayer();
                             break;
                         case (int)UserAction.ban_player:
+                            BanPlayer();
+                            break;
+                        case (int)UserAction.unban_player:
+                            UnbanPlayer();
                             break;
                         case (int)UserAction.show_all_players:
-                            Console.Clear();
                             ShowAllPlayers();
+                            break;
+                        case (int)UserAction.get_back_to_real_life:
+                            ByeBye(ref _yourFuneral);
                             break;
                         default:
                             Usability.WriteLineInStyle("That's a wrong number. Try again, fool");
@@ -94,68 +102,151 @@ namespace CsRealLearning
                 Usability.WriteLineInStyle($"Player {input} succesfully added", ConsoleColor.Green);
             }
 
-            public void RemovePlayer()
+            public bool TryToSelectPlayerById(out Player selectedPlayer)
             {
-                Player selectedPlayer = null;
-
-                Console.Clear();
+                selectedPlayer = null;
 
                 if (players.Count == 0)
                 {
                     Usability.WriteLineInStyle("The players list is empty", ConsoleColor.Blue);
+                    return false;
                 }
-                else
+
+                ShowAllPlayers();
+                Console.Write("\nEnter player ID: ");
+
+                if (!int.TryParse(Console.ReadLine(), out int playerInput))
                 {
-                    ShowAllPlayers();
-                    Console.Write("\nEnter player ID you want to remove: ");
+                    Usability.WriteLineInStyle($"Input is incorrect. Enter a number next time");
+                    return false;
+                }
 
-                    if (!int.TryParse(Console.ReadLine(), out int playerInput))
+                for (int i = 0; i < players.Count; i++)
+                {
+                    if (players[i].Id == playerInput)
                     {
-                        Usability.WriteLineInStyle($"Input is incorrect. Enter a number next time");
-
+                        selectedPlayer = players[i];
+                        return true;
                     }
-                    else
-                    {
-                        foreach (Player player in players)
-                        {
-                            if (player.Id == playerInput)
-                            {
-                                selectedPlayer = player;
-                            }
-                        }
 
-                        if (selectedPlayer != null)
+                    if (i == players.Count - 1)
+                    {
+                        Usability.WriteLineInStyle($"Player with ID: {playerInput} wasn't found");
+                    }
+                }
+
+                return false;
+            }
+
+            public void RemovePlayer()
+            {
+                Console.Clear();
+
+                if (TryToSelectPlayerById(out Player selectedPlayer))
+                {
+                    Usability.WriteLineInStyle($"Player with id {selectedPlayer.Id} " +
+                        $"(name: {selectedPlayer.Name}) succesfully removed", ConsoleColor.Green);
+                    players.Remove(selectedPlayer);
+                }
+            }
+
+            public void BanPlayer()
+            {
+                Console.Clear();
+
+                if (TryToSelectPlayerById(out Player playerToBan))
+                {
+                    Usability.WriteLineInStyle($"Player with id {playerToBan.Id} " +
+                        $"(name: {playerToBan.Name}) succesfully BANNED", ConsoleColor.Green);
+
+                    foreach (Player player in players)
+                    {
+                        if (playerToBan.Id == player.Id)
                         {
-                            Usability.WriteLineInStyle($"Player with id {selectedPlayer.Id} (name: {selectedPlayer.Name}) succesfully removed", ConsoleColor.Green);
-                            players.Remove(selectedPlayer);
-                        }
-                        else
-                        {
-                            Usability.WriteLineInStyle($"Player with id {playerInput} not found", ConsoleColor.Red);
+                            if (player.IsBanned == true)
+                            {
+                                Usability.WriteLineInStyle("Again, get that", ConsoleColor.Green);
+                                player.IsBanned = true;
+                            }
+                            else
+                            {
+                                player.IsBanned = true;
+                            }
                         }
                     }
                 }
             }
 
-            public void BanPlayer(int Id)
+            public void UnbanPlayer()
             {
+                Console.Clear();
+
+                if (TryToSelectPlayerById(out Player playerToUnban))
+                {
+                    foreach (Player player in players)
+                    {
+                        if (playerToUnban.Id == player.Id)
+                        {
+                            if (player.IsBanned == false)
+                            {
+                                Usability.WriteLineInStyle("He isn't even banned, man", ConsoleColor.White);
+                            }
+                            else
+                            {
+                                player.IsBanned = false;
+                                Usability.WriteLineInStyle($"Player with id {playerToUnban.Id} " +
+                                    $"(name: {playerToUnban.Name}) succesfully unbanned", ConsoleColor.Green);
+                            }
+                        }
+                    }
+                }
             }
 
             public void ShowAllPlayers()
             {
-                foreach (Player player in players)
+                Console.Clear();
+
+                if (players.Count != 0)
                 {
-                    player.ShowInfo();
+                    foreach (Player player in players)
+                    {
+                        player.ShowInfo();
+                    }
                 }
+                else
+                {
+                    Usability.WriteLineInStyle("Player list is empty", ConsoleColor.Blue);
+                }
+            }
+
+            public void ByeBye(ref bool yourFuneral)
+            {
+                Usability.WriteLineInStyle($"Are you sure? Life is cruel and full of suffering", ConsoleColor.White);
+                string playerInput = Console.ReadLine();
+
+                if (playerInput.ToLower() == "yes")
+                {
+                    Usability.WriteLineInStyle($"Are you like sure sure? It's not going to be pretty", ConsoleColor.Red);
+                    playerInput = Console.ReadLine();
+
+                    if (playerInput.ToLower() == "yes")
+                    {
+                        Usability.WriteLineInStyle("okay, it's your funeral, bye-bye", ConsoleColor.DarkRed);
+                        yourFuneral = true;
+                        return;
+                    }
+                }
+
+                Usability.WriteLineInStyle($"That was a correct desision. Good job", ConsoleColor.Green);
             }
         }
 
         class Player
         {
             private static int _lastId;
+            public bool IsBanned;
 
             public int Id { get; private set; }
-            public bool IsBanned { get { return false; } private set { } }
             public string Name { get; private set; }
             public int Level { get { return 1; } private set { } }
 
@@ -163,6 +254,7 @@ namespace CsRealLearning
             {
                 Name = name;
 
+                IsBanned = false;
                 Id = _lastId++;
             }
 
@@ -190,6 +282,13 @@ namespace CsRealLearning
                 Console.ForegroundColor = color;
                 Console.WriteLine(style);
                 Console.ResetColor();
+            }
+
+            public static void PressAnything(string text = "\npress anything to continue")
+            {
+                Usability.WriteLineInStyle(text, ConsoleColor.DarkYellow);
+                Console.ReadKey();
+                Console.Clear();
             }
         }
     }
