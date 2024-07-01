08ec6038462d49942d4e8bbb10e87b04427029a6
alex_k63 <38862874+Alex633@users.noreply.github.com>
2024-07-01 13:07:55 +0300

    Create 1.1 Variables v2.cs

---
+++ 1.1 Variables v2.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _170000Courses
+{
+    internal class Program
+    {
+        //Попрактикуйтесь в создании переменных. Объявите 10 переменных и присвойте им значения.
+        //Минимум 5 типов переменных должны быть разными. Одна из переменных должна быть типа bool.
+
+        static void Main()
+        {
+            string greetings = "hello world";
+            int number = 1;
+            byte tinyNumber = 2;
+            short negativeMediumNumber = -32000;
+            long largeNumber = 1000000;
+            ulong positiveLargeNumber = 500000;
+            double decimalNumber = 0.1;
+            float decimalNumberTwo = 0.6f;
+            char symbol = '#';
+            bool isToggled = true;
+        }
+    }
+}
