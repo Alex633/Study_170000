e497b04a5ddec654049bd79632da93a83a375961
alex_k63 <38862874+Alex633@users.noreply.github.com>
2024-07-03 12:16:23 +0300

    Create 1.7 Hospital v2.cs

---
+++ 1.7 Hospital v2.cs
@@ -0,0 +1,40 @@
+namespace millionDollarsCourses
+{
+    using System;
+
+    //   Легенда: 
+    //Вы заходите в поликлинику и видите огромную очередь из пациентов, вам нужно рассчитать время ожидания в очереди.
+    //Формально: 
+    //Пользователь вводит кол-во людей в очереди.  
+    //Фиксированное время приема одного человека всегда равно 10 минутам.
+    //Пример ввода: Введите кол-во пациентов: 14
+    //Пример вывода: "Вы должны отстоять в очереди 2 часа и 20 минут."
+    //Примечание:
+    //- при расчетах надо использовать только переменные.
+    //Если число не присваивается переменной, то в большинстве случаев это число магическое (исключение 0 и 1, но не во всех ситуациях).
+    //- повторные расчеты так же стоит выносить в переменные
+
+    internal class Program
+    {
+        static void Main()
+        {
+            const int appointmentDurationMinutes = 10;
+
+            Console.WriteLine("Welcome to your doctor appointment\n" +
+                "How many patiens are currently in line?");
+            int queueOfPatients = Convert.ToInt32(Console.ReadLine());
+            int waitingTime = appointmentDurationMinutes * queueOfPatients;
+
+            if (waitingTime < 0)
+                Console.WriteLine($"Amount of people can't be negative, dummy. Or can it?");
+            else if (waitingTime == 0)
+                Console.WriteLine("Looks like you are in luck, come on in");
+            else if (waitingTime < 30)
+                Console.WriteLine($"It's only {waitingTime} minutes. Not that bad, right?");
+            else if (waitingTime < 120)
+                Console.WriteLine($"Waiting time is {waitingTime} minutes. We are so sorry");
+            else
+                Console.WriteLine($"I hope you didn't have any plans for today. You will have to wait for {waitingTime} minutes");
+        }
+    }
+}
