e515e1eafadbf659be9b2d1a76a75f9a4f2d9ed7
alex_k63 <38862874+Alex633@users.noreply.github.com>
2024-07-05 12:38:39 +0300

    Update 2.1.6 Console Menu v2.cs

--- 2.1.6 Console Menu v2.cs
+++ 2.1.6 Console Menu v2.cs
@@ -2,16 +2,16 @@ namespace millionDollarsCourses
 {
     using System;
 
-//    При помощи всего, что вы изучили, создать приложение, которое может обрабатывать команды.
-//    Т.е.вы создаете меню, ожидаете ввода нужной команды, после чего выполняете действие, которое присвоено этой команде.
-//    Программа не должна завершаться после ввода, пользователь сам должен выйти из программы при помощи команды.
-//    Меню должно содержать следующие команды:
-//- пара команд на вывод разного текста
-//- команда показать случайное число
-//- команда очистить консоль
-//    - команда выхода
-//    Если решение строится на switch, то принято работать с константами (в остальных случаях объявляются переменные).
-//    Подробнее вы можете изучить в статье Использование констант
+    //    При помощи всего, что вы изучили, создать приложение, которое может обрабатывать команды.
+    //    Т.е.вы создаете меню, ожидаете ввода нужной команды, после чего выполняете действие, которое присвоено этой команде.
+    //    Программа не должна завершаться после ввода, пользователь сам должен выйти из программы при помощи команды.
+    //    Меню должно содержать следующие команды:
+    //- пара команд на вывод разного текста
+    //- команда показать случайное число
+    //- команда очистить консоль
+    //    - команда выхода
+    //    Если решение строится на switch, то принято работать с константами (в остальных случаях объявляются переменные).
+    //    Подробнее вы можете изучить в статье Использование констант
 
     internal class Program
     {
@@ -20,6 +20,7 @@ namespace millionDollarsCourses
             const string CommandHelp = "help";
             const string CommandExit = "exit";
             const string CommandOutputText = "output text";
+            const string CommandSaySomethingNice = "say something nice about me";
             const string CommandDisplayRandomNumber = "display number";
             const string CommandClearConsole = "clear console";
 
@@ -39,6 +40,7 @@ namespace millionDollarsCourses
                     case CommandHelp:
                         Console.WriteLine($"\ntype {CommandHelp} to get help\n" +
                             $"type {CommandOutputText} to get me to write something\n" +
+                            $"type {CommandSaySomethingNice} to ask me to say something nice about you\n" +
                             $"type {CommandDisplayRandomNumber} to see the coolest random number\n" +
                             $"type {CommandClearConsole} to forget everything\n" +
                             $"type {CommandExit} to leave me (please don't)\n");
@@ -49,6 +51,9 @@ namespace millionDollarsCourses
                         break;
                     case CommandOutputText:
                         Console.WriteLine($"\ni don't know what text do you want me to write here");
+                        break;
+                    case CommandSaySomethingNice:
+                        Console.WriteLine($"\ni mean i don't know you. you might be a terrible human being");
                         break;
                     case CommandDisplayRandomNumber:
                         randomNumber = random.Next();
