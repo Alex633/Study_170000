9cd9c0b4ce459149b1a185cce04ec922068db1b1
alex_k63 <38862874+Alex633@users.noreply.github.com>
2024-07-12 14:39:08 +0300

    Create 3.8 Shift array elements v2.cs

---
+++ 3.8 Shift array elements v2.cs
@@ -0,0 +1,43 @@
+//  Дан массив чисел. Нужно его сдвинуть циклически на указанное пользователем значение позиций влево, не используя других массивов. 
+//  Пример для сдвига один раз: { 1, 2, 3, 4} => { 2, 3, 4, 1}
+
+using System;
+
+namespace millionDollarsCourses
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int tempNumber = 0;
+
+            Console.Write($"Shift value: ");
+            int shiftSteps = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Before: ");
+
+            foreach (int i in numbers)
+                Console.Write($"{i} ");
+
+            Console.WriteLine();
+
+            for (int i = 0; i < shiftSteps; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    tempNumber = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = tempNumber;
+                }
+            }
+
+            Console.WriteLine($"After: ");
+
+            foreach (int i in numbers)
+                Console.Write($"{i} ");
+
+            Console.WriteLine();
+        }
+    }
+}
