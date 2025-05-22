b306e1c2ae406379716d1252e762c40951f3f496
alex_k63 <38862874+Alex633@users.noreply.github.com>
2025-05-22 12:53:04 +0300

    Create 5.1 Explanatory dictionary v2.cs

---
+++ 5.1 Explanatory dictionary v2.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    private static void Main()
+    {
+        Dictionary<string, int> words = new Dictionary<string, int>()
+        {
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 }
+        };
+
+        Console.Write("Input key: ");
+        TryOutputValue(Console.ReadLine(), words);
+    }
+
+    public static void TryOutputValue(string key, Dictionary<string, int> dictionary)
+    {
+        if (dictionary.TryGetValue(key, out int result))
+            WriteLineAt($"{result}", foregroundColor: ConsoleColor.Green);
+        else
+            WriteLineAt($"Your search ({key}) did not match any keys", foregroundColor: ConsoleColor.Red);
+    }
+
+    #region Helper
+    public static void WriteLineAt(object element, int? yPosition = null, int? xPosition = null,
+        ConsoleColor foregroundColor = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black)
+    {
+        Console.ForegroundColor = foregroundColor;
+        Console.BackgroundColor = backgroundColor;
+
+        if (yPosition.HasValue || xPosition.HasValue)
+            Console.SetCursorPosition(xPosition != null ? xPosition.Value : 0, yPosition != null ? yPosition.Value : 0);
+
+        Console.WriteLine(element);
+
+        Console.ResetColor();
+    }
+    #endregion
+}
