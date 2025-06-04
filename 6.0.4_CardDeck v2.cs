788f17880908151199fa5bbbe5e97e742442f9a7
alex_k63 <38862874+Alex633@users.noreply.github.com>
2025-06-04 17:37:17 +0300

    Update 6.0.4_CardDeck v2.cs

--- 6.0.4_CardDeck v2.cs
+++ 6.0.4_CardDeck v2.cs
@@ -1,89 +1,270 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-enum Suit
+enum SuitName
 {
     Clubs = 1,
+    Spades,
     Diamonds,
     Hearts,
-    Spades
 }
 
-enum Value
+enum ValueName
 {
     Ace = 1,
-    Two = 2,
-    Three = 3,
-    Four = 4,
-    Five = 5,
-    Six = 6,
-    Seven = 7,
-    Eight = 8,
-    Nine = 9,
-    Ten = 10,
-    Jack = 11,
-    Queen = 12,
-    King = 13
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
 }
 
 public class Program
 {
     static void Main()
     {
+        Console.OutputEncoding = Encoding.UTF8;
         //Database database = new Database();
 
         ConsoleMenu menu = new ConsoleMenu();
+        Deck deck = new Deck();
 
-        menu.Run();
+        //menu.Run();
+
+
+        deck.Render("Deck");
+        Console.ReadKey();
     }
 }
 
 class Table
 {
+    private Deck _deck;
 
+    private Player _player;
+
+    public Table()
+    {
+        _deck = new Deck();
+        _player = new Player();
+    }
+
+    public void DealCards()
+    {
+
+    }
 }
 
 class Player
 {
+    private List<Card> _hand;
 
+    public Player()
+    {
+        _hand = new List<Card>();
+    }
 }
 
 class Deck
 {
-    public Deck()
-    {
-        Initialize();
-    }
+    public const int Size = 52;
 
     private Stack<Card> _cards;
 
-    public void Shuffle()
+    public Deck()
     {
-
+        Populate();
+        Shuffle();
     }
 
-    private void Initialize()
+    public Card DrawOne()
     {
-        _cards = new Stack<Card>();
+        if (_cards.Count == 0)
+            throw new Exception("The deck is empty.");
 
-        for (int i = 0; i < 4; i++)
+        return _cards.Pop();
+    }
+
+    public void Render(string title, int rowSize = 4)
+    {
+        Helper.WriteTitle(title);
+
+        int cardCount = 0;
+
+        foreach (Card card in _cards)
         {
-            _cards.Push(new Card(Value.Ace, Suit.Spades));
+            card.Draw();
+            Console.Write(" ");
+
+            cardCount++;
+
+            if (cardCount % rowSize == 0)
+                Console.WriteLine();
         }
+    }
+
+    public void Shuffle()
+    {
+        var tempDeck = new List<Card>(Size);
+
+        for (int i = 0; i < Size; i++)
+            tempDeck.Add(_cards.Pop());
+
+        for (int i = Size - 1; i > 0; i--)
+        {
+            int randomIndex = Helper.GetRandomInt(0, (i + 1));
+
+            Card tempCard = tempDeck[i];
+
+            tempDeck[i] = tempDeck[randomIndex];
+            tempDeck[randomIndex] = tempCard;
+        }
+
+        _cards = new Stack<Card>(tempDeck);
+    }
+
+    private void Populate()
+    {
+        _cards = new Stack<Card>(Size);
+        
+        foreach (ValueName value in Enum.GetValues(typeof(ValueName)))
+            foreach (SuitName suit in Enum.GetValues(typeof(SuitName)))
+                _cards.Push(new Card(value, suit));
     }
 }
 
 class Card
 {
-    public Card(Value value, Suit suit)
-    {
+    private ConsoleColor _color => Suit.Color;
 
+    public Card(ValueName value, SuitName suit)
+    {
+        Value = new Value(value);
+        Suit = new Suit(suit);
     }
 
     public Value Value { get; private set; }
-
     public Suit Suit { get; private set; }
+
+    public void Draw()
+    {
+        Helper.WriteAt($"[ {Suit.Symbol} {Value.Symbol} ]",
+            foregroundColor: _color, isNewLine: false);
+    }
 }
+
+class Suit
+{
+    private string _name;
+
+    public Suit(SuitName suit)
+    {
+        _name = suit.ToString();
+        Symbol = ToSymbol(suit);
+
+        Color = suit == SuitName.Clubs || suit == SuitName.Spades ? 
+            ConsoleColor.White : ConsoleColor.DarkRed;
+    }
+
+    public string Symbol { get; private set; }
+    public ConsoleColor Color { get; private set; }
+
+
+    private string ToSymbol(SuitName suit)
+    {
+        switch (suit)
+        {
+            case SuitName.Clubs:
+                return "♣";
+
+            case SuitName.Diamonds:
+                return "♦";
+
+            case SuitName.Hearts:
+                return "♥";
+
+            case SuitName.Spades:
+                return "♠";
+
+            default:
+                throw new Exception("Unknown suit");
+        }
+    }
+}
+
+class Value
+{
+    private string _name;
+
+    public Value(ValueName value)
+    {
+        _name = value.ToString();
+        Symbol = ToSymbol(value);
+        Weight = (int)value;
+    }
+
+    public string Symbol { get; private set; }
+
+    public int Weight { get; private set; }
+
+    public string ToSymbol(ValueName value)
+    {
+        switch (value)
+        {
+            case ValueName.Ace:
+                return "A ";
+
+            case ValueName.Two:
+                return "2 ";
+
+            case ValueName.Three:
+                return "3 ";
+
+            case ValueName.Four:
+                return "4 ";
+
+            case ValueName.Five:
+                return "5 ";
+
+            case ValueName.Six:
+                return "6 ";
+
+            case ValueName.Seven:
+                return "7 ";
+
+            case ValueName.Eight:
+                return "8 ";
+
+            case ValueName.Nine:
+                return "9 ";
+
+            case ValueName.Ten:
+                return "10";
+
+            case ValueName.Jack:
+                return "J ";
+
+            case ValueName.Queen:
+                return "Q ";
+
+            case ValueName.King:
+                return "K ";
+
+            default:
+                throw new Exception("Unknown value");
+        }
+    }
+}
+
 
 class ConsoleMenu
 {
@@ -147,15 +328,15 @@ class ConsoleMenu
         const int CommandFull = 5;
         const int CommandExit = 6;
 
-        _items = new Dictionary<int, MenuItem>()
-        {
-            [CommandAdd] = new MenuItem("Add gamer", _database.AddGamer),
-            [CommandRemove] = new MenuItem("Remove gamer", _database.RemoveGamer),
-            [CommandBan] = new MenuItem("Ban gamer", _database.BanGamer),
-            [CommandUnban] = new MenuItem("Unban gamer", _database.UnbanGamer),
-            [CommandFull] = new MenuItem("Output full database", () => _database.TryOutputFull()),
-            [CommandExit] = new MenuItem("Exit", Exit),
-        };
+        //_items = new Dictionary<int, MenuItem>()
+        //{
+        //    [CommandAdd] = new MenuItem("Add gamer", _database.AddGamer),
+        //    [CommandRemove] = new MenuItem("Remove gamer", _database.RemoveGamer),
+        //    [CommandBan] = new MenuItem("Ban gamer", _database.BanGamer),
+        //    [CommandUnban] = new MenuItem("Unban gamer", _database.UnbanGamer),
+        //    [CommandFull] = new MenuItem("Output full database", () => _database.TryOutputFull()),
+        //    [CommandExit] = new MenuItem("Exit", Exit),
+        //};
     }
 }
 
@@ -180,6 +361,13 @@ class MenuItem
 
 class Helper
 {
+    private static readonly Random _rng = new Random();
+
+    public static int GetRandomInt(int minValue, int maxValue)
+    {
+        return _rng.Next(minValue, maxValue);
+    }
+
     public static string ReadString(string helpText, ConsoleColor primary = ConsoleColor.Cyan, ConsoleColor secondary = ConsoleColor.Black)
     {
         ConsoleColor backgroundColor = Console.BackgroundColor;
