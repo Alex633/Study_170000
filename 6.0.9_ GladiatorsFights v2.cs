107b3de8b17a96370e2cdebe1b25b0c1d2f3786b
alex_k63 <38862874+Alex633@users.noreply.github.com>
2025-07-04 22:37:49 +0300

    Update 6.0.9_ GladiatorsFights v2.cs

--- 6.0.9_ GladiatorsFights v2.cs
+++ 6.0.9_ GladiatorsFights v2.cs
@@ -31,7 +31,7 @@ class Arena
 
     public Arena()
     {
-        IntializeFighters();
+        InitializeFighters();
         InitializeCommands();
     }
 
@@ -59,7 +59,7 @@ class Arena
 
         Console.WriteLine();
         int firstFighter = Helper.ReadIntInRange("Выберите первого воина: ", _fighterClasses.Count) - 1;
-        int secondFighter = Helper.ReadInt("Выберите второго воина: ") - 1;
+        int secondFighter = Helper.ReadIntInRange("Выберите второго воина: ", _fighterClasses.Count) - 1;
 
         DetermineInitiative(firstFighter, secondFighter);
         Helper.WaitForKeyPress();
@@ -157,7 +157,7 @@ class Arena
         }
     }
 
-    private void IntializeFighters()
+    private void InitializeFighters()
     {
         _fighterClasses = new List<Fighter>()
         {
@@ -165,6 +165,8 @@ class Arena
             new Thief(),
             new Dualist(),
             new Berserk(),
+            new Paladin(),
+            new Wizard(),
         };
     }
 
@@ -186,15 +188,15 @@ class Assassin : Fighter
     private int _criticalChancePercentage;
     private double _criticalMultiplier;
 
-    public Assassin() : base("Убийца", 22, 2, 0)
+    public Assassin() : base("Убийца", 22, 2, 1)
     {
-        _criticalMultiplier = 8.5;
-        _criticalChancePercentage = 33;
+        _criticalMultiplier = 6;
+        _criticalChancePercentage = 45;
     }
 
     public override void Attack(IDamageable subject)
     {
-        bool isCriticalStrike = Helper.PerformDiceCheck($"{Name} кидает кубик на критическую атаку", _criticalChancePercentage);
+        bool isCriticalStrike = Helper.PerformDiceCheck($"{Name} пытается пройти на критическую атаку", _criticalChancePercentage);
         int damage = isCriticalStrike == false ? Damage : (int)(Damage * _criticalMultiplier);
         string strikeAdditionalInfo = isCriticalStrike ? "критически " : "";
 
@@ -215,16 +217,16 @@ class Assassin : Fighter
 
 class Thief : Fighter
 {
-    private int _dodgeChangePercentage;
+    private int _dodgeChancePercentage;
 
-    public Thief() : base("Вор", 10, 4, 0)
+    public Thief() : base("Вор", 7, 4, 1)
     {
-        _dodgeChangePercentage = 75;
+        _dodgeChancePercentage = 75;
     }
 
     public override void TakeDamage(int damage)
     {
-        bool isDodge = Helper.PerformDiceCheck($"{Name} кидает кубик на уворот", _dodgeChangePercentage);
+        bool isDodge = Helper.PerformDiceCheck($"{Name} пытается пройти проверку на уворот", _dodgeChancePercentage);
 
         if (isDodge == false)
             base.TakeDamage(damage);
@@ -235,7 +237,7 @@ class Thief : Fighter
     public override void ShowInfo()
     {
         base.ShowInfo();
-        Console.WriteLine($", уворот: {_dodgeChangePercentage}%");
+        Console.WriteLine($", уворот: {_dodgeChancePercentage}%");
     }
 
     public override Fighter Clone()
@@ -245,11 +247,13 @@ class Thief : Fighter
 class Dualist : Fighter
 {
     private int _extraAttackFrequency;
+    private int _extraAttacks;
     private int _attackCount;
 
     public Dualist() : base("Дуалист", damage: 3)
     {
-        _extraAttackFrequency = 2;
+        _extraAttackFrequency = 3;
+        _extraAttacks = 3;
     }
 
     public override void Attack(IDamageable subject)
@@ -257,19 +261,21 @@ class Dualist : Fighter
         base.Attack(subject);
         _attackCount++;
 
-        if (_attackCount == _extraAttackFrequency)
+        if (_attackCount >= _extraAttackFrequency)
         {
-            Helper.WriteAt($"{Name} атакует еще раз, так как это {_attackCount} атака");
-            _attackCount = 0;
-            base.Attack(subject);
-        }
+            Helper.WriteAt($"{Name} атакует еще {_extraAttacks} раза, так как это {_attackCount} атака");
 
+            for (int i = 0; i < _extraAttacks; i++)
+                base.Attack(subject);
+
+            _attackCount = 0;
+        }
     }
 
     public override void ShowInfo()
     {
         base.ShowInfo();
-        Console.WriteLine($", атакует дважды каждую {_extraAttackFrequency} атаку");
+        Console.WriteLine($", атакует {_extraAttacks} дополнительных раза каждую {_extraAttackFrequency} атаку");
     }
 
     public override Fighter Clone()
@@ -281,10 +287,13 @@ class Berserk : Fighter
     private int _rageCurrent;
     private int _rageToDoubleDamage;
     private int _ragePerHit;
+    private int _rageDamageModifier;
 
-    public Berserk() : base("Берсерк", 100, 3, -10)
+    public Berserk() : base("Берсерк", 100, 1, -8)
     {
-        _rageToDoubleDamage = 10;
+        _rageDamageModifier = 5;
+        _rageCurrent = 0;
+        _rageToDoubleDamage = 8;
         _ragePerHit = 2;
     }
 
@@ -292,50 +301,177 @@ class Berserk : Fighter
     {
         base.TakeDamage(damage);
 
-        if (IsAlive)
-            ApplyRage(damage);
+        ApplyRage();
     }
 
     public override void ShowInfo()
     {
         base.ShowInfo();
-        Console.WriteLine($", накапливает {_ragePerHit} ярости при получении урона. Ярость удваивает атаку каждые {_rageToDoubleDamage} очков ярости");
+        Console.WriteLine($", накапливает {_ragePerHit} ярости при получении урона. Ярость увеличивает атаку в {_rageDamageModifier} раз каждые {_rageToDoubleDamage} очков ярости");
     }
 
     public override Fighter Clone()
     => new Berserk();
 
-    private void ApplyRage(int damage)
+    private void ApplyRage()
     {
+        if (IsAlive == false)
+            return;
+
         _rageCurrent += _ragePerHit;
         Helper.WriteAt($"{Name} накапливает ярость до {_rageCurrent}");
 
         if (_rageCurrent % _rageToDoubleDamage == 0)
         {
-            int damageModifier = 2;
-
-            Damage *= damageModifier;
+            Damage *= _rageDamageModifier;
             Helper.WriteAt($"{Name} в ярости. Атака увеличена до {Damage}", foregroundColor: ConsoleColor.Yellow);
         }
     }
 }
 
+class Paladin : Fighter
+{
+    private int _blessingCurrent;
+    private int _maxBlessing;
+    private int _blessingPerHit;
+    private int _blessingHealthRestoration = 8;
+
+    public Paladin() : base("Паладин", damage: 2, armor: 2)
+    {
+        _blessingCurrent = 0;
+        _maxBlessing = 15;
+        _blessingPerHit = 5;
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+
+        ApplyBlessing();
+    }
+
+    public override void ShowInfo()
+    {
+        base.ShowInfo();
+        Console.WriteLine($", накапливает {_blessingPerHit} благославления при получении урона. " +
+            $"При достижении {_maxBlessing} восстанавливает {_blessingHealthRestoration} ХП");
+    }
+
+    public override Fighter Clone()
+    => new Paladin();
+
+    private void ApplyBlessing()
+    {
+        if (IsAlive == false)
+            return;
+
+        _blessingCurrent += _blessingPerHit;
+        Helper.WriteAt($"{Name} накапливает благославление до {_blessingCurrent}");
+
+        if (_blessingCurrent >= _maxBlessing)
+        {
+            _blessingCurrent = 0;
+
+            Health += _blessingHealthRestoration;
+            Helper.WriteAt($"{Name} восстанавливает {_blessingHealthRestoration} ХП", foregroundColor: ConsoleColor.Yellow);
+        }
+    }
+}
+
+class Wizard : Fighter
+{
+    private int _manaCurrent;
+    private int _maxMana;
+    private int _manaRestoration;
+
+    private int _fireBallDamage;
+    private int _fireBallCost;
+
+    public Wizard() : base("Волшебник", health: 17, damage: 1, armor: 0)
+    {
+        _maxMana = 7;
+        _manaCurrent = _maxMana;
+        _manaRestoration = 1;
+
+        _fireBallDamage = 14;
+        _fireBallCost = 4;
+    }
+
+    public override void Attack(IDamageable subject)
+    {
+        RestoreMana();
+
+        if (_manaCurrent >= _fireBallCost)
+            UseFireball(subject);
+        else
+            base.Attack(subject);
+    }
+
+    public override void ShowInfo()
+    {
+        base.ShowInfo();
+        Console.WriteLine($", мана: {_maxMana}. Использует огненный шар (наносит {_fireBallDamage} урона, требует {_fireBallCost} маны). Восстанавливает {_manaRestoration} маны в ход");
+    }
+
+    public override Fighter Clone()
+    => new Wizard();
+
+    private void UseFireball(IDamageable subject)
+    {
+        _manaCurrent -= _fireBallCost;
+
+        Helper.WriteAt($"({Health}) {Name} кидает огненный шар и наносит {_fireBallDamage} урона");
+
+        subject.TakeDamage(_fireBallDamage);
+    }
+
+    private void RestoreMana()
+    {
+        if (IsAlive == false)
+            return;
+
+        _manaCurrent += _manaRestoration;
+
+        if (_manaCurrent > _maxMana)
+            _manaCurrent = _maxMana;
+    }
+}
+
 abstract class Fighter : IDamageable
 {
-    public int Damage { get; protected set; }
-    public string Name { get; private set; }
-    public int Health { get; private set; }
-    public int Armor { get; private set; }
-
-    public bool IsAlive => Health > 0;
+    protected int _maxHealth;
+    private int _currentHealth;
 
     public Fighter(string name, int health = 30, int damage = 4, int armor = 1)
     {
         Name = name;
-        Health = health;
+
+        _maxHealth = health;
+        _currentHealth = health;
+
         Damage = damage;
         Armor = armor;
     }
+
+    public int Damage { get; protected set; }
+    public string Name { get; private set; }
+    public int Armor { get; private set; }
+
+    public int Health
+    {
+        get => _currentHealth;
+        protected set
+        {
+            _currentHealth = value > _maxHealth ? _maxHealth : value;
+
+            if (value < 0)
+                _currentHealth = 0;
+        }
+    }
+
+    public bool IsAlive => Health > 0;
+
+    public abstract Fighter Clone();
 
     public virtual void Attack(IDamageable subject)
     {
@@ -347,13 +483,15 @@ abstract class Fighter : IDamageable
     public virtual void TakeDamage(int damage)
     {
         if (IsAlive == false)
+        {
             Helper.WriteAt($"(X_X) {Name} уже мертв. Оставьте его в покое");
+            return;
+        }
 
-        int minHealth = 0;
-        int actualDamage = Math.Max(minHealth, damage - Armor);
+        int actualDamage = Math.Max(0, damage - Armor);
         string damageRedaction = Armor > 0 ? $"(-{Armor}) " : "";
 
-        Health = Math.Max(minHealth, Health - actualDamage);
+        Health -= actualDamage;
 
         if (actualDamage > 0)
             Helper.WriteAt($"({Health}) {Name} получает {actualDamage} {damageRedaction}урона");
@@ -363,18 +501,16 @@ abstract class Fighter : IDamageable
         FallDramaticallyIfDead();
     }
 
-    private void FallDramaticallyIfDead()
-    {
-        if (IsAlive == false)
-            Helper.WriteAt($"({Health}) {Name} драматически падает на землю (X_X)");
-    }
-
     public virtual void ShowInfo()
     {
         Console.Write($"{Name}: {Health} ХП, {Damage} урона, {Armor} брони");
     }
 
-    public abstract Fighter Clone();
+    private void FallDramaticallyIfDead()
+    {
+        if (IsAlive == false)
+            Helper.WriteAt($"({Health}) {Name} драматически падает на землю (X_X)");
+    }
 }
 
 class CommandLineInterface
